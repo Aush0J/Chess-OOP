e85e893386880c008abd31fc8e0308435a130080
Aush0J <63893173+Aush0J@users.noreply.github.com>
2020-12-07 14:48:11 +0800

    Rook class
    finished making the Rook and added slight adjustments to Bishop

--- src/com/chess/engine/pieces/Rook.java
+++ src/com/chess/engine/pieces/Rook.java
@@ -12,10 +12,10 @@ import static com.chess.engine.board.Move.*;
 import com.chess.engine.board.Tile;
 import com.google.common.collect.ImmutableList;
 
-public class Bishop extends Piece {
-    private final static int[] possibleMoveVectorCoordinates = {-9, -7, 7 ,9};
+public class Rook extends Piece {
+    private final static int[] possibleMoveVectorCoordinates = {-8, -1, 1 ,8};
 
-    Bishop(final int piecePosition, final Color pieceColor) {
+    Rook(final int piecePosition, final Color pieceColor) {
         super(piecePosition, pieceColor);
     }
 
@@ -52,9 +52,9 @@ public class Bishop extends Piece {
     }
 
     private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
-        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7);
+        return BoardUtils.FIRST_COLUMN[currentPosition] && candidateOffset == -1;
     }
     private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset){
-        return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 9);
+        return BoardUtils.EIGHTH_COLUMN[currentPosition] && candidateOffset == 1;
     }
 }
